2b1aa383cd1bd43baad9dc22141e4d708adc92c6
vik170 <per.vikene@gmail.com>
2021-01-23 13:04:41 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,23 +22,40 @@ const cat = {
 
 //Question 2
 
-const heading = document.querySelector("h3"):
-heading.innerText = "Updated heading":
+const heading = document.querySelector("h3");
+heading.innerText = "Updated heading";
 
 //Question 3
 
-heading.style.fontSize = "2em":
+heading.style.fontSize = "2em";
 
 //Question 4
 
-const heading = document.querySelector("h3.subheading"):
+heading.className = "subheading";
 
 //Question 5
 
-const paragraphs = document.querySelectorAll("p"):
-
+const paragraphs = document.querySelectorAll("p");
+for (i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].style.color = "red";
+}
+    
 //Question 6
 
+const resultsContainer = document.querySelector(".results")
+resultsContainer.innerHTML = "<p>New paragraph</p>";
+resultsContainer.style.backgroundColor = "yellow"
+
 //Question 7
+
+function list(value) {
+    for (i = 0; i < value.length; i++) {
+        console.log(value[i].name)
+    }
+}
+
+list(cats)
+
+
 
 //Question 8
