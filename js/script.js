9ef271fd08a6cf4a102d39d59286310564a62ea3
vik170 <per.vikene@gmail.com>
2021-01-23 10:32:47 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,3 +12,33 @@ const cats = [
     }
 ];
 
+//Question 1
+// ikke ferdig
+const cat = {
+    complain: function() {
+        console.log("Meow!")
+    }
+  }
+
+//Question 2
+
+const heading = document.querySelector("h3"):
+heading.innerText = "Updated heading":
+
+//Question 3
+
+heading.style.fontSize = "2em":
+
+//Question 4
+
+const heading = document.querySelector("h3.subheading"):
+
+//Question 5
+
+const paragraphs = document.querySelectorAll("p"):
+
+//Question 6
+
+//Question 7
+
+//Question 8
