b0f08d73868570645f29794abe31453fc91c014e
vik170 <per.vikene@gmail.com>
2021-01-23 20:27:29 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 const cats = [
-    {
-        name: "Blob",
-        age: 10
-    },
-    {
-        name: "Harold",
-    },
-    {
-        name: "Blurt",
-        age: 21
-    }
+	{
+		name: "Blob",
+		age: 10,
+	},
+	{
+		name: "Harold",
+	},
+	{
+		name: "Blurt",
+		age: 21,
+	},
 ];
 
 //Question 1
-// ikke ferdig
+
 const cat = {
-    complain: function() {
-        console.log("Meow!")
-    }
-  }
+	complain: function () {
+		console.log("Meow!");
+	}
+};
 
 //Question 2
 
@@ -37,25 +37,47 @@ heading.className = "subheading";
 
 const paragraphs = document.querySelectorAll("p");
 for (i = 0; i < paragraphs.length; i++) {
-    paragraphs[i].style.color = "red";
+	paragraphs[i].style.color = "red";
 }
-    
+
 //Question 6
 
-const resultsContainer = document.querySelector(".results")
+const resultsContainer = document.querySelector(".results");
 resultsContainer.innerHTML = "<p>New paragraph</p>";
-resultsContainer.style.backgroundColor = "yellow"
+resultsContainer.style.backgroundColor = "yellow";
 
 //Question 7
 
 function list(value) {
-    for (i = 0; i < value.length; i++) {
-        console.log(value[i].name)
-    }
+	for (i = 0; i < value.length; i++) {
+		console.log(value[i].name);
+	}
 }
 
-list(cats)
-
-
+list(cats);
 
 //Question 8
+
+let html = "";
+
+function createCats(cats) {
+	for (i = 0; i < cats.length; i++) {
+		let age = "Age unkown";
+
+		if (cats[i].age) {
+			age = cats[i].age;
+		}
+
+		html += `<div>
+                    <h5>${cats[i].name}</h5>
+                    <p>${age}</p>
+                </div>`;
+    }
+    
+	return html;
+}
+
+const updatedHTML = createCats(cats);
+
+const gameContainer = document.querySelector(".cat-container");
+gameContainer.innerHTML = updatedHTML;
